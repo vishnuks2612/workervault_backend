ff4f9ff039a69f552470b5ad9b4d4cc4b1d47ed4
vishnuks2612@gmail.com <vishnuks2612@gmail.com>
2024-01-25 13:29:48 +0530

    second

--- workervault/models.py
+++ workervault/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class WorkerVaultModel(models.Model):
-    userid = models.AutoField(primary_key=True, default="")
+    userid = models.AutoField(primary_key=True)
     name = models.CharField(default = "", max_length = 50)
     phoneno = models.IntegerField(default = "")
     emailid = models.EmailField(default = "", max_length = 50)
